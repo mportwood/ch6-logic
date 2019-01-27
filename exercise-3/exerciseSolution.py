2a84dcb9b93c4ac42d34eb6e73578d201144418c
Matt Portwood <portwood.matt@gmail.com>
2019-01-27 13:16:47 -0800

    Prep Top-Level Script for class demo

--- exercise-3/exerciseSolution.py
+++ exercise-3/exerciseSolution.py
@@ -20,15 +20,13 @@ def print_hot_or_cold(target, guess):
     dist = abs(target - guess)
     if dist == 0: #same number
         print("Got it!")
-    elif dist == 1: 
+    elif dist <= 1: 
         print("Scalding Hot")
     #if distance >= 2 and distance <=3 #This works but there's a better way, change above to elif
     elif dist <= 3:  #You've already checked above
         print ("very warm")
     elif dist <= 5: 
         print("warm")
-    #otherwise, do nothing... if dist > 5, then nothing happens
-    # until I add the following lines
     elif dist <=8: 
         print("cold")
     elif dist <=13: 
@@ -38,8 +36,7 @@ def print_hot_or_cold(target, guess):
 
 #at this point is does nothing...need to call function we created
 
-#call function
-#print_hot_or_cold(-12, 10)
+
 
 # Define a function `guess_number()` that takes a single argument (a target number)
 # and prompts the user for a guess using the `input()` method. Your function should
@@ -51,7 +48,11 @@ def print_hot_or_cold(target, guess):
 # or by simply calling your `guess_number() method again IF the user didn't get
 # the answer right. The later is an example of **recursion**.
 
-
+def guess_number(target): 
+    guess = int(input("Guess a number!"))
+    print_hot_or_cold(target, guess)
+    if guess != target: 
+        guess_number(target)
 
 # If the file is run as a top-level script, your script should pick a random number
 # between 1 and 50 as the target and then start the game. You should inform the
@@ -59,10 +60,15 @@ def print_hot_or_cold(target, guess):
 
 #_______Script v. Module________
 # Write all functions at top and add this to bottom
-
 ##This part means only run if run from the command line
-if __name__ == "__main__": #or != "__main__" don't run if run in command line
-    print_hot_or_cold(50, 49)
+
+if __name__ == "__main__":
+    #print_hot_or_cold(50, 49)
+    #guess_number(50)
+    import random 
+    target = random.randint(1,50)
+    print("Try guessing a random # from 1-50!")
+    guess_number(target)
 
 ##__name__ is a special var that refers to the name of the script that is currently running
 ##not the name of the file (as a top level script)
@@ -70,6 +76,3 @@ if __name__ == "__main__": #or != "__main__" don't run if run in command line
 ##This way others can import and run the cool functions you built
 ##This makes your code reusable. (so like the print...(12, 10) above won't run)
 ##imagine like if you created a package and tried to import it like "import math"
-
-##Might be helpful to have a demo.
-##!!!!!look up how to explain __name__
